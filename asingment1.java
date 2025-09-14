80d11b2ee0f4c410ccefd4bb19013cc40a39eb07
McMilan123 <milan.azlen@gmail.com>
2025-09-14 18:50:18 +0200

    Add files via upload

---
+++ asingment1.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void
+    main(String[] args) {
+        novaMetoda();
+        for (int i = 0; i < 36; i++) {
+           System.out.println("Hello World");
+
+        }
+    }
