84cbc7dcc8c029f40de11e7bb1f1bb39b263ef19
McMilan123 <milan.azlen@gmail.com>
2025-09-16 19:24:19 +0200

    Uradjen zadatak "Assignment1"

---
+++ Assignment1.java
@@ -0,0 +1,8 @@
+public class Assignment1 {
+
+    public static void nekaNovaMetoda(){
+        for(int i = 0; i < 36; i++){
+            System.out.println("Hello World");
+        }
+    }
+}
--- asingment1.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void
-    main(String[] args) {
-        novaMetoda();
-        for (int i = 0; i < 36; i++) {
-           System.out.println("Hello World");
-
-        }
-    }
